b0c15ffcc80b54ff9e132a1cbe89b62df1b55c6b
Chandan Singh <chandan@Chandans-MacBook-Air.local>
2020-12-23 16:16:40 +0530

    Add implementation to find possible positions for pawns.

--- src/main/service/impl/PawnsPositionFinder.java
+++ src/main/service/impl/PawnsPositionFinder.java
@@ -1,14 +1,19 @@
 package service.impl;
 
 import models.BoardPosition;
+import service.Mover;
 import service.PositionsFinder;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+
+import static models.MoveDirection.Forward;
 
 public class PawnsPositionFinder implements PositionsFinder {
   @Override
   public List<BoardPosition> getPossibleBoardPositionsFrom(BoardPosition startingPosition) {
-    return new ArrayList<>();
+    BoardPosition afterForwardMove = Mover.move(startingPosition, Forward, 1);
+    return new ArrayList<>(Collections.singletonList(afterForwardMove));
   }
 }
